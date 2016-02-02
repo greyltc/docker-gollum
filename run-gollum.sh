b14a5f5d12c68058946ad5a2f00b07857a8a15d6
Grey Christoforo <grey@christoforo.net>
2016-02-02 17:17:41 +0000

    add run and setup files

---
+++ run-gollum.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+gollum --port 80 --allow-uploads dir /root/wiki&
+
---
+++ setup-gollum.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pacman -S base-devel ruby icu git
+gem install gollum
+gem install redcarpet org-ruby
+echo 'PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH' > /etc/profile.d/ruby.sh
+mkdir /root/wiki
+cd /root/wiki
+git init
