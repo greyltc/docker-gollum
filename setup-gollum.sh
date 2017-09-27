5b240e9496627a65f6b4fc03815cd1be11c68668
Grey Christoforo <grey@christoforo.net>
2017-09-27 14:08:30 +0100

    remove unneeded installs

--- setup-gollum.sh
+++ setup-gollum.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-pacman -S --needed --noconfirm base-devel ruby icu git
+pacman -S --needed --noconfirm base-devel ruby git
 gem install gollum
-gem install redcarpet org-ruby
 echo "PATH=$(ruby -e 'print Gem.user_dir')/bin:"'$PATH' > /etc/profile.d/ruby.sh
 mkdir /root/wiki
 cd /root/wiki
