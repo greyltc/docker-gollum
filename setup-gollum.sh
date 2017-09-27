8676b60f1f81389d72e800370523ee5617affa8f
Grey Christoforo <grey@christoforo.net>
2017-09-27 14:01:46 +0100

    fix ruby gems path

--- setup-gollum.sh
+++ setup-gollum.sh
@@ -3,7 +3,7 @@
 pacman -S --needed --noconfirm base-devel ruby icu git
 gem install gollum
 gem install redcarpet org-ruby
-echo 'PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH' > /etc/profile.d/ruby.sh
+echo "PATH=$(ruby -e 'print Gem.user_dir')/bin:"'$PATH' > /etc/profile.d/ruby.sh
 mkdir /root/wiki
 cd /root/wiki
 git init
