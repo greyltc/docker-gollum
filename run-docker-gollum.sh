5c41193c91f71d399a2c826095c080959d3619ab
Grey Christoforo <grey@christoforo.net>
2018-01-19 19:47:09 +0100

    Update run-docker-gollum.sh

--- run-docker-gollum.sh
+++ run-docker-gollum.sh
@@ -6,7 +6,7 @@ docker run \
   --detach=true \
   --restart='always' \
   --name gollum \
-  --env GOLLUM_OPTS='--bare --allow-uploads dir' \
+  --env GOLLUM_OPTS='--bare --allow-uploads dir --show-all' \
   --publish 4567:4567 \
   --mount type=bind,source="$HOME/wiki",target=/root/wiki \
   --tty=false \
