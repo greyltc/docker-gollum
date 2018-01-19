3079b01584c1407d9ec720232df833768e10cb47
Grey Christoforo <grey@christoforo.net>
2018-01-19 11:01:00 +0100

    Update setup-gollum.sh

--- setup-gollum.sh
+++ setup-gollum.sh
@@ -2,8 +2,12 @@
 
 pacman -S --needed --noconfirm base-devel ruby git
 echo "PATH=$(ruby -e 'print Gem.user_dir')/bin:"'$PATH' > /etc/profile.d/ruby.sh
+echo "export PATH" >> /etc/profile.d/ruby.sh
+chmod +x /etc/profile.d/ruby.sh
 source /etc/profile.d/ruby.sh
+
 gem install gollum
-mkdir /root/wiki
-cd /root/wiki
-git init
+
+#mkdir /root/wiki
+#cd /root/wiki
+#git init
